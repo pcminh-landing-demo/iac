1e4e35127d0a4ca2586d152ccf54dd08dd9dea97
Phan Cong Minh <pcminh@users.noreply.github.com>
2025-10-29 15:39:51 +0700

    Move outputs from main.tf to outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,12 @@
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
 output "configure_kubectl" {
   description = "Configure kubectl: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
   value       = "aws eks --region ${var.aws_region} update-kubeconfig --name ${module.eks.cluster_name}"
+}
+
+output "eks_cluster_arn" {
+  value = module.eks.cluster_arn
 }
